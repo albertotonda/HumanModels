5b18f7eaa690496dd73a922e668523156da2180d
albertotonda <alberto.tonda@gmail.com>
2021-04-28 23:30:59 +0200

    Minor modifications.

--- humanmodels/__init__.py
+++ humanmodels/__init__.py
@@ -6,7 +6,6 @@ scikit-learn compatible human-defined models for classification and regression
 from .humanmodels import HumanRegressor
 from .humanmodels import HumanClassifier
 
-
 __version__ = "0.0.6"
 
 __all__ = [
